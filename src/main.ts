86c4b1f47967af43315ea1078970e947c3b2461c
Johan Camilo Delgado Gomez <kmilogomez154@gmail.com>
2022-08-23 08:28:20 -0500

    [develop] hotfix: set process.env.port in man

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,7 @@ async function bootstrap() {
   app.enableCors()
 
   swaggerSetup(app)
-  console.log(process.env)
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
